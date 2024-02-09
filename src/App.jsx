e4bdb2e6f30e5b4a8a8298c8bf389be358c3cb64
muqaddasazahra <muqaddasazahra@gmail.com>
2024-02-09 14:18:03 +0500

    Added Player Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import Player from "./components/Player"
 function App() {
   
 
   return (
-    <h1>Coming Soon..........</h1>
+    <main>
+      <div id="game-container">
+       <ol id="players">
+       <Player name="Player 1" symbol="X"></Player>
+       <Player name="Player 2" symbol="O"/>
+       </ol>
+      </div>
+
+    </main>
   )
 }
 
