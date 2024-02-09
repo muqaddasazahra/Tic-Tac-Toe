a4ce36629b57d0a80dc820cfe2267c649718cc45
muqaddasazahra <muqaddasazahra@gmail.com>
2024-02-09 14:42:05 +0500

    Player Name Edit functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 export default function Player({name, symbol})
-{
+{   
+    const [isEditing, setIsEditing]=useState(false);
+
+    function handleClick()
+    {
+    setIsEditing(true);
+    }
     return (
         <li>
         <span>
-        <span className="player-name">{name}</span>
+        {(isEditing)? <input type="text" required/>: <span className="player-name">{name}</span> }
+       
         <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleClick}>Edit</button>
        </li>
     );
 
