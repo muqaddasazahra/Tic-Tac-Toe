7c0f221d86e853a36c887aa053a6af41080bdcb9
muqaddasazahra <muqaddasazahra@gmail.com>
2024-03-07 15:30:35 +0500

    changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Tic-Tac-Toe-React",
   plugins: [react()],
-  base:"/Tic-Tac-Toe/"
+
 })
